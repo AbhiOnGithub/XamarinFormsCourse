e6a5e0c5381fed1a6549d131a7b3f31bdb5e01bd
abhiongithub <innovative.abhishek@hotmail.com>
2016-10-18 02:46:04 +0530

    Added Basic Code for Layout Demo of Module 4

---
+++ LayoutsDemo/LayoutsDemo/LayoutsDemo/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using LayoutsDemo.LayoutPages;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace LayoutsDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            List<Page> listOfPages = new List<Page>()
+            {
+                new StackLayoutPage(),
+                new GridLayoutPage(),
+                new RelativeLayoutPage(),
+                new ScrollLayoutPage(),
+                new AbsoluteLayoutPage()
+            };
+
+            listPages.ItemsSource = listOfPages;
+            listPages.ItemSelected += ListPages_ItemSelected;
+
+        }
+
+
+        void ListPages_ItemSelected(object sender,SelectedItemChangedEventArgs e)
+        {
+            if(e.SelectedItem != null)
+            {
+                Navigation.PushAsync((Page)e.SelectedItem);
+            }
+            listPages.SelectedItem = null;
+        }
+    }
+}
